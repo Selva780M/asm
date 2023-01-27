cbde5635df8bcd78fcbd88d6964632cf81a736f9
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-01-27 21:51:41 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -1,2 +1,2 @@
 import streamlit as st
-st.write(f'<iframe src="https://www.Google.com/" frameborder="0" scrolling="yes" webkitAllowFullScreen="true" mozallowfullscreen="true" allowFullScreen="true" height="400" width="100%"></iframe>',unsafe_allow_html=True)
+st.write(f'<iframe src="https://www.Google.com/" frameborder="0" scrolling="yes" webkitAllowFullScreen="true" mozallowfullscreen="true" allowFullScreen="true" height="1920" width="100%"></iframe>',unsafe_allow_html=True)
