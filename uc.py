b7f192feda668a0c0b72bdfb056cdb66d49d6113
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-01-31 14:38:58 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -1,16 +1,42 @@
-import cv2
 import streamlit as st
-#st.write(f'<iframe src="http://www.google.com/custom?q=&btnG=Search" frameborder="0" scrolling="yes" webkitAllowFullScreen="true" mozallowfullscreen="true" allowFullScreen="true" height="1920" width="100%"></iframe>',unsafe_allow_html=True)
-#st.write(f'<iframe id="if1" width="100%" height="254" style="visibility:visible" src="http://www.google.com/custom?q=&btnG=Search"></iframe>',unsafe_allow_html=True)
-import sys
-from PyQt5.QtCore import *
-from PyQt5.QtWebEngineWidgets import *
-from PyQt5.QtWidgets import QApplication
- 
-app = QApplication(sys.argv)
- 
-web = QWebEngineView()
-web.load(QUrl("https://stackoverflow.com/"))
-web.show()
- 
-sys.exit(app.exec_())
+
+st.write("Paper Trading Simulation")
+
+# initialize portfolio
+portfolio = {'symbol': [], 'shares': [], 'price': [], 'total': []}
+
+# add stocks to portfolio
+def add_stock(symbol, shares, price):
+    portfolio['symbol'].append(symbol)
+    portfolio['shares'].append(shares)
+    portfolio['price'].append(price)
+    portfolio['total'].append(shares * price)
+
+# remove stocks from portfolio
+def remove_stock(symbol):
+    idx = portfolio['symbol'].index(symbol)
+    portfolio['symbol'].pop(idx)
+    portfolio['shares'].pop(idx)
+    portfolio['price'].pop(idx)
+    portfolio['total'].pop(idx)
+
+# calculate current portfolio value
+def calculate_value():
+    return sum(portfolio['total'])
+
+# UI for adding stocks
+if st.checkbox("Add Stock"):
+    symbol = st.text_input("Symbol")
+    shares = int(st.text_input("Shares"))
+    price = float(st.text_input("Price"))
+    add_stock(symbol, shares, price)
+
+# UI for removing stocks
+if st.checkbox("Remove Stock"):
+    symbol = st.text_input("Symbol")
+    remove_stock(symbol)
+
+# display portfolio
+st.write("Portfolio")
+st.write(portfolio)
+st.write("Value: $", calculate_value())
