f04090df29d3b7e2c89eed2c89531a6d4924a87d
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-01-31 15:21:53 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -16,7 +16,7 @@ def main():
                      m = alice.get_scrip_info(alice.get_instrument_by_symbol('INDICES', i))
                      st.write(f"Nifty50:{m['LTP']}Change:{m['Change']} %{m['PerChange']}")
        except Exception as e:              
-              st.write(f"Error"{e})
+              st.write(f"Error",{e})
        sleep(1)
 
 
