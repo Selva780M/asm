278f40b6b3b8c0f8c97be0168ae1bc3a59f2c887
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-01 18:13:36 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -108,7 +108,7 @@ if len(df['STOCK']) > 0:
 			df100 = pd.DataFrame()
 			df1 = pd.Series(em,name='ltp')
 			df100 = pd.concat([df,df1],axis=1)
-			df100.to_csv('token.csv',index = False)						
+			#df100.to_csv('token.csv',index = False)						
 			with placeholder100.container():
 				st.write(f'<h1 style="color:#33ff33;font-size:40px;">{f"POSITION"}</h1>', unsafe_allow_html=True)
 				st.table(df100)
