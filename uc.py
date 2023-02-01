83c35b2dc0879a9206f58763ee419621c66f6d8f
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-01 16:39:22 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -103,9 +103,8 @@ if len(df['STOCK']) > 0:
 				m = alice.get_scrip_info(alice.get_instrument_by_symbol('NFO',i))				
 				lt = m['LTP'] 								
 				em.append(lt)				
-			st.write(em)
-			em = pd.DataFrame(em)
-			pandas.concat([df, em], axis=1)
+			st.write(em)			
+			df = pandas.concat([df, em], axis=1)
 			#df = df.append(em, ignore_index = True)
 			#df = df.append(em, ignore_index = True)
 			#df.to_csv('token.csv',index = False)
