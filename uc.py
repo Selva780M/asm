964cb3ee7c04431bd8558eb6560354b8ba9ff90a
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-01 21:16:36 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -115,7 +115,7 @@ if len(df['STOCK']) > 0:
 		df100 = pd.DataFrame()
 		df1 = pd.Series(em,name='LTP')
 		df100 = pd.concat([df,df1],axis=1)
-		df100['P_L'] = (df100['LTP'] - 100) * df100['QTY']  #df100['ENTRY'] * 								
+		df100['P_L'] = (df100['LTP'] - df100['ENTRY']) * df100['QTY'] 								
 		with placeholder100.container():
 			st.write(f'<h1 style="color:#33ff33;font-size:40px;">{f"Position"}</h1>', unsafe_allow_html=True)
 			st.table(df100)		
