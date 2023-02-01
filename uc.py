3e92adb58f4ab0ef901cc17edcacd1acedbb2402
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-01 17:52:44 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -94,24 +94,24 @@ with st.form("opt_form"):
 				df = df.append(new_data, ignore_index = True)
 				df.to_csv('token.csv',index = False)
 				st.balloons()
-		
-if len(df['STOCK']) > 0:	
-	while True:
-		em = []
-		for i in df['STOCK']:
-			try:
-				m = alice.get_scrip_info(alice.get_instrument_by_symbol('NFO',i))				
-				lt = float(m['LTP'])								
-				em.append(lt)				
-			except Exception as e:
-				st.write(f"Er.",{e})					
-		df100 = pd.DataFrame()
-		df1 = pd.Series(em,name='ltp')
-		df100 = pd.concat([df,df1],axis=1)
-		df100.to_csv('token.csv',index = False)
-		placeholder100 = st.empty()
-		with placeholder100.container():
-			st.write(f'<h1 style="color:#33ff33;font-size:40px;">{f"POSITION"}</h1>', unsafe_allow_html=True)
-			st.table(df100)
-		time.sleep(1)
-
+def main():
+	if len(df['STOCK']) > 0:	
+		while True:
+			em = []
+			for i in df['STOCK']:
+				try:
+					m = alice.get_scrip_info(alice.get_instrument_by_symbol('NFO',i))				
+					lt = float(m['LTP'])								
+					em.append(lt)				
+				except Exception as e:
+					st.write(f"Er.",{e})					
+			df100 = pd.DataFrame()
+			df1 = pd.Series(em,name='ltp')
+			df100 = pd.concat([df,df1],axis=1)
+			df100.to_csv('token.csv',index = False)
+			placeholder100 = st.empty()
+			with placeholder100.container():
+				st.write(f'<h1 style="color:#33ff33;font-size:40px;">{f"POSITION"}</h1>', unsafe_allow_html=True)
+				st.table(df100)
+			time.sleep(1)
+main()
