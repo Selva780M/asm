be98de5a7017f4822c0e5e88348938d2870392af
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-01 09:40:53 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -63,9 +63,11 @@ with st.form("opt_form"):
 			if user_OPTION == "call":
 				call_strike = spot - (50)
 				n_call = alice.get_instrument_for_fno(exch="NFO", symbol="NIFTY", expiry_date=expiry_date, is_fut=False,strike=call_strike, is_CE=True)
-				st.write(n_call.name)				
-				st.balloons()
-				new_data = {"DATE" : DATE ,"NAME": user_USER, "STOCK" : n_call.name,  "ENTRY" : float(0.00), "QTY" : int(user_LOT), "STOPLOSS" : float(0.00 - user_STOP), "TARGET" : float(0.00 + user_TARGET), "P_L" : 0, "%" : 0 }
+				st.write(n_call.name)
+				s = (alice.get_scrip_info(alice.get_instrument_by_symbol('NFO', "n_call.name")))
+				entry = s['LTP']
+				st.balloons()				
+				new_data = {"DATE" : DATE ,"NAME": user_USER, "STOCK" : n_call.name,  "ENTRY" : float(entry), "QTY" : int(user_LOT), "STOPLOSS" : float(0.00 - user_STOP), "TARGET" : float(0.00 + user_TARGET), "P_L" : 0, "%" : 0 }
 				df = df.append(new_data, ignore_index = True)
 				df.to_csv('token.csv',index = False)
 			if  user_OPTION == "put":
