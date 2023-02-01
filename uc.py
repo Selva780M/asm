dd51c93f247db35475305c6fd176025a5718ba74
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-01 15:04:24 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -110,13 +110,13 @@ if len(df['STOCK']) > 0:
 			for i in df['STOCK']:				
 				m = alice.get_scrip_info(alice.get_instrument_by_symbol('NFO',i))				
 				lt = m['LTP']
-				sign = np.where( float(lt) > float(0.00) , float(lt) , 0 )
+				sign = np.where(float(lt) > float(0.00) , float(lt) , 0 )
 				record = {"ltp": sign}
-				ltp = em.append(record)
+				em.append(record)
 				#st.write(f"{i} {m['LTP']}")			
-			df = df.append(ltp, ignore_index = True)
-			df.to_csv('token.csv',index = False)
-			st.table(df)
+			df1 = df.append(em, ignore_index = True)
+			df1.to_csv('token.csv',index = False)
+			st.table(df1)
 		except Exception as e:
 			st.write(f"Er.",{e}) 
 		time.sleep(1)
