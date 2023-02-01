e076592c0383b4743df4273228cb401ea5e40748
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-01 14:56:35 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -110,7 +110,7 @@ if len(df['STOCK']) > 0:
 			for i in df['STOCK']:				
 				m = alice.get_scrip_info(alice.get_instrument_by_symbol('NFO',i))				
 				lt = m['LTP']
-				sign = np.where( lt > 0 , float(lt) , 0 )
+				sign = np.where( float(lt) > float(0.00) , float(lt) , 0 )
 				ltp = em.append(sign)
 				#st.write(f"{i} {m['LTP']}")			
 			df = df.append(ltp, ignore_index = True)
