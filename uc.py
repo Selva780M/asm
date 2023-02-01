123dcfb1a363bb8013a11b389374f04d999bd18c
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-01 11:58:07 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -104,7 +104,7 @@ with st.form("opt_form"):
 st.write(f'<h1 style="color:#33ff33;font-size:40px;">{f"POSITION"}</h1>', unsafe_allow_html=True)		
 if len(df['STOCK']) > 0:
 	try:
-		for i in range(len(df['STOCK'])):
+		for i in len(df['STOCK'].values):
 			#m = alice.get_scrip_info(alice.get_instrument_by_symbol('NFO',i))
 			st.write(i)
 			#st.write(f"{i} {m['LTP']}")
