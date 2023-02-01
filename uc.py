f6f4a524e1456969288ea28c358c6ae167636bea
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-01 18:10:57 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -111,6 +111,6 @@ if len(df['STOCK']) > 0:
 			df100.to_csv('token.csv',index = False)						
 			with placeholder100.container():
 				st.write(f'<h1 style="color:#33ff33;font-size:40px;">{f"POSITION"}</h1>', unsafe_allow_html=True)
-			st.table(df100)
+				st.table(df100)
 			time.sleep(1)
 
