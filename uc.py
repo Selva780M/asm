486d7789e0b602f07460af6c547d57a1ba6222ba
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-02 00:57:06 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -131,7 +131,8 @@ with st.form("opt_form"):
 				df100 = pd.concat([df,df1],axis=1)
 				df100['P_L'] = (df100['LTP'] - 1000) #*df100['ENTRY']) * df100['QTY']
 				with placeholder12.container():
-					st.table(df100.style.applymap(subset=['NAME','P_L'],col))
+					ab = df[['NAME','P_L']]
+					st.table(ab.style.applymap(col))
 				A = df100.style.applymap(col)
 				with placeholder100.container():
 					st.write(f'<h1 style="color:#33ff33;font-size:40px;">{f"Position"}</h1>', unsafe_allow_html=True)
