66a77662177a538cdafc3446057aaf0688cff366
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-02 13:47:29 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -143,14 +143,14 @@ with st.form("opt_form"):
 				for i in range(0,len(df100.index)):
 					st.dataframe(df100)
 					st.write(df100.iloc[i,7])
-					if(df100.iloc[i,7]) > (df100.iloc[i,6]):
+					if(df100.iloc[i,6]) < (df100.iloc[i,7]):
 						st.write('loop1')
 						df2 = {"DATE" : df100.iloc[i]['DATE'] ,"NAME": df100.iloc[i]['NAME'], "STOCK" : df100.iloc[i]['STOCK'],  "ENTRY" : df100.iloc[i]['ENTRY'], "QTY" : df100.iloc[i]['QTY'], "STOPLOSS" : df100.iloc[i]['STOPLOSS'], "TARGET" : df100.iloc[i]['TARGET'], "LTP" : df100.iloc[i]['LTP'],"P_L" :df100.iloc[i]['P_L']}						
 						df5.append(df2, ignore_index = True)
 						df5.to_csv('trade.csv',index = False)
 						df100 = df100.drop(df.index[i])
 						#df100 = df100.drop(index=[i], axis=0)
-					if(df100.iloc[i,5]) < (df100.iloc[i,7]):
+					if(df100.iloc[i,5]) > (df100.iloc[i,7]):
 						st.write('loop2')
 						df3 = {"DATE" : df100.iloc[i]['DATE'] ,"NAME": df100.iloc[i]['NAME'], "STOCK" : df100.iloc[i]['STOCK'],  "ENTRY" : df100.iloc[i]['ENTRY'], "QTY" : df100.iloc[i]['QTY'], "STOPLOSS" : df100.iloc[i]['STOPLOSS'], "TARGET" : df100.iloc[i]['TARGET'], "LTP" : df100.iloc[i]['LTP'],"P_L" :df100.iloc[i]['P_L']}
 						df5.append(df3, ignore_index = True)
