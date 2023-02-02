2c69e5dc92a4b16543b002eec7a78fea28252f46
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-02 17:41:38 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -116,7 +116,7 @@ with st.form("opt_form"):
 				try:
 					for i in df['STOCK']:
 						m = alice.get_scrip_info(alice.get_instrument_by_symbol('NFO',i))				
-						lt = m['LTP']								
+						lt = float(m['LTP'])								
 						em.append(lt)
 				except Exception as e:
 					st.write(f"Er.",{e})					
