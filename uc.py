fa196f163031693d5f40a8da62abda0329777d4e
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-02 18:03:31 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -154,11 +154,11 @@ with st.form("opt_form"):
 						st.metric("Rs", f"{im()}" , f"{PL}")
 				with placeholder100.container():
 					st.write(f'<h1 style="color:#33ff33;font-size:40px;">{f"Position"}</h1>', unsafe_allow_html=True)					
-					A = df100.style.format(subset=["ENTRY","QTY","STOPLOSS","TARGET","LTP","P_L" ], formatter="{:.1f}").applymap(col)
+					A = df100.style.format(subset=["ENTRY","QTY","STOPLOSS","TARGET","LTP","P_L" ], formatter="{:.2f}").applymap(col)
 					st.table(A)							
 				with placeholder101.container():
 					st.write(f'<h1 style="color:#33ff33;font-size:40px;">{f"Complete Trade"}</h1>', unsafe_allow_html=True)
-					B = df5.style.applymap(col)
+					B = df5.style.format(subset=["ENTRY","QTY","STOPLOSS","TARGET","LTP","P_L" ], formatter="{:.2f}").applymap(col)					
 					st.table(B)	
 				time.sleep(1)
 		
