45eba1738ef8384468b2abe71638491030c63d33
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-02 18:34:16 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -128,16 +128,16 @@ with st.form("opt_form"):
 				with placeholder12.container():					
 				#	ab = pd.DataFrame([df100.NAME, df100.P_L]).transpose()					
 				#	st.table(ab.style.applymap(col))								
-					with st.form("Fund):
-						     col1, col2 = st.columns(2)
-						     st.write('Availble Margin')
-						     st.write('Rs.10000')						     
-						     with col1:
-						     	st.write('Availble Cash')
-						     	st.write('Rs.10000')
-						     with col2:
-						     	st.write('Margin Used')
-						     	st.write('Rs.5500')
+					with st.form("Fund"):
+						col1, col2 = st.columns(2)
+						st.write('Availble Margin')
+						st.write('Rs.10000')
+						with col1:
+							st.write('Availble Cash')
+							st.write('Rs.10000')
+						with col2:
+							st.write('Margin Used')
+							st.write('Rs.5500')
 				for i in range(0,len(df100.index)):					
 					if(df100.iloc[i,6]) < (df100.iloc[i,7]) and (df100.iloc[i,1] not in df5['NAME'].tolist()) and (df100.iloc[i,2] not in df5['STOCK'].tolist()):						
 						df2 = {"DATE" : df100.iloc[i]['DATE'] ,"NAME": df100.iloc[i]['NAME'], "STOCK" : df100.iloc[i]['STOCK'],  "ENTRY" : df100.iloc[i]['ENTRY'], "QTY" : df100.iloc[i]['QTY'], "STOPLOSS" : df100.iloc[i]['STOPLOSS'], "TARGET" : df100.iloc[i]['TARGET'], "LTP" : df100.iloc[i]['LTP'],"P_L" :df100.iloc[i]['P_L']}						
