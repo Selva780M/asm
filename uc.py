4f1670498adbfd46e290fb061fb6434bee452126
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-02 11:15:48 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -135,7 +135,7 @@ with st.form("opt_form"):
 					st.table(ab.style.applymap(col))
 				A = df100.style.applymap(col)
 				for i in range(len(df100)):
-					df55 = {"DATE" : df100.iloc[i][1] ,"NAME": df100.iloc[i][2], "STOCK" : df100.iloc[i][3],  "ENTRY" : df100.iloc[i][4], "QTY" : df100.iloc[i][5], "STOPLOSS" : df100.iloc[i][6], "TARGET" : df100.iloc[i][7], "LTP" : df100.iloc[i][8],"P_L" :df100.iloc[i][9]}
+					df55 = {"DATE" : df100.iloc[i]['DATE'] ,"NAME": df100.iloc[i]['NAME'], "STOCK" : df100.iloc[i]['STOCK'],  "ENTRY" : df100.iloc[i]['ENTRY'], "QTY" : df100.iloc[i]['QTY'], "STOPLOSS" : df100.iloc[i]['STOPLOSS'], "TARGET" : df100.iloc[i]['TARGET'], "LTP" : df100.iloc[i]['LTP'],"P_L" :df100.iloc[i]['P_L']}
 					st.write(df55)
 				with placeholder100.container():
 					st.write(f'<h1 style="color:#33ff33;font-size:40px;">{f"Position"}</h1>', unsafe_allow_html=True)
