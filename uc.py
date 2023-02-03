ca8f5c01dd728b2abca20ea07be49a3e9f6575ae
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-03 12:32:55 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -154,10 +154,13 @@ with st.form("opt_form"):
 						df.drop([i], inplace = True)
 				with col33:
 					with placeholder01.container():
-						st.write(f'<h1 style="color:#33ff33;font-size:25px;">{"*_Profit / Loss_*"}</h1>', unsafe_allow_html=True)
+						st.write(f'<h1 style="color:#33ff33;font-size:25px;">{f'*_Profit / Loss_*'}</h1>', unsafe_allow_html=True)
 						PL = round((df100.loc[df100['NAME'] == str(user_USER) , 'P_L'].sum()),1)
-						st.metric("Rs", f"{im()}" , f"{PL}")
-						st.metric("%",f"{im()}" , f"{PL}")
+						col1 , col2 = st.columns(2)
+						with col1:
+							st.metric("Rs", f"{im()}" , f"{PL}")
+						with col2:
+							st.metric("%",f"{im()}" , f"{PL}")
 				with placeholder100.container():
 					st.write(f'<h1 style="color:#33ff33;font-size:40px;">{f"Position"}</h1>', unsafe_allow_html=True)					
 					A = df100.style.format(subset=["ENTRY","QTY","STOPLOSS","TARGET","LTP","P_L" ], formatter="{:.2f}").applymap(col)
