ecc1b132fd669450bfc9d1f341c9ab1ea775c900
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-04 10:28:55 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -14,8 +14,8 @@ df5 = pd.read_csv('./trade.csv')
 #------------------------------------------------------
 placeholder1 = st.empty()	
 with placeholder1.container():
-	st.header('👋Hey,iam👉:blue[_ALGO PAPER TRADE_] :sunglasses:')
-	st.subheader('Welcome!! Mr._Selvakumar_ lets go...⏰')	
+	st.header('*_👋Hey,Im👉:blue[_ALGO PAPER TRADE_] :sunglasses:_*')
+	st.subheader('*_Welcome!! Mr._Selvakumar_ lets go...⏰_*')	
 	con1, con2 ,con3 = st.columns(3)
 	with con1:
 		st.markdown(f""" *_Date:_* {DATE}""")
