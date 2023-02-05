89835a3cbe8bf25974346e5fcb3afd29041820da
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-05 08:24:45 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -201,7 +201,14 @@ with st.form("opt_form"):
 				with placeholder100.container():
 					st.write(f'<h1 style="color:#33ff33;font-size:40px;">{f"Position"}</h1>', unsafe_allow_html=True)					
 					A = df100.style.format(subset=["ENTRY","QTY","STOPLOSS","TARGET","LTP","P_L" ], formatter="{:.2f}").applymap(col)
-					st.table(A)						
+					st.table(A)
+					if cl:
+						df100.drop([num], inplace = True)
+						df100.to_csv('trade.csv',index = False)
+					if dl:
+						for i in range(0,len(df100.index)):
+							df100.drop([i], inplace = True)
+						df100.to_csv('token.csv',index = False)
 					col11, col22, col33,col44 = st.columns(4)									
 					with col11:
 						cl = st.form_submit_button('👉*_Clear Row_*',on_click =cl)
@@ -213,13 +220,6 @@ with st.form("opt_form"):
 						st.write(f'<h1 style="color:#33ff33;font-size:40px;">{f"Complete Trade"}</h1>', unsafe_allow_html=True)
 						B = df5.style.format(subset=["ENTRY","QTY","STOPLOSS","TARGET","LTP","P_L" ], formatter="{:.2f}").applymap(col)					
 						st.table(B)
-					if cl:
-						df100.drop([num], inplace = True)
-						df100.to_csv('trade.csv',index = False)
-					if dl:
-						for i in range(0,len(df100.index)):
-							df100.drop([i], inplace = True)
-						df100.to_csv('token.csv',index = False)
 					df.to_csv('token.csv',index = False)
 			time.sleep(1)
 
