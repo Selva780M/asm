f18a7f3f417985f035d7ce973033e5a3a332bd3f
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-05 09:23:36 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -208,11 +208,11 @@ with st.form("opt_form"):
 					st.table(B)
 			if cr:
 				df100.drop([num], inplace = True)
-				df100.to_csv('token.csv',index = False)
+				#df100.to_csv('token.csv',index = False)
 			if cl:
 				for i in range(0,len(df100.index)):
 					df100.drop([i], inplace = True)
-				df100.to_csv('token.csv',index = False)
+				#df100.to_csv('token.csv',index = False)
 			df.to_csv('token.csv',index = False)
 			time.sleep(1)
 
