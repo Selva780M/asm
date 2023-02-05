ee245ae9e9a2f7b799345efa2c80506a47060857
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-05 07:23:44 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -204,19 +204,19 @@ with st.form("opt_form"):
 				st.table(A)						
 				col11, col22, col33,col44 = st.columns(4)									
 				with col11:
-					cl = st.form_submit_button('👉 *_Clear Row_*')
+					cl = st.form_submit_button('👉*_Clear Row_*')
 				with col22:
 					num = st.number_input('*_EnterRow No_*', min_value=0, max_value=len(df5.index), value=1, step=1, format=None, key=None)					
 				with col44:
-					dl  = st.form_submit_button('👉 *_Clear ALL_*')
+					dl  = st.form_submit_button('👉*_Clear ALL_*')
 			with placeholder101.container():
 				st.write(f'<h1 style="color:#33ff33;font-size:40px;">{f"Complete Trade"}</h1>', unsafe_allow_html=True)
 				B = df5.style.format(subset=["ENTRY","QTY","STOPLOSS","TARGET","LTP","P_L" ], formatter="{:.2f}").applymap(col)					
 				st.table(B)
-			if cl():
+			if cl:
 				df100.drop([num], inplace = True)
 				df100.to_csv('trade.csv',index = False)
-			if dl :
+			if dl:
 				for i in range(0,len(df100.index)):
 					df100.drop([i], inplace = True)
 				df100.to_csv('token.csv',index = False)
