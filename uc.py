b51ca5524ea3fdc6c48ae4d026a8d16b949da86b
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-05 08:18:43 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -208,7 +208,7 @@ with st.form("opt_form"):
 					with col22:
 						num = st.number_input('*_EnterRow No_*', min_value=0, max_value=len(df5.index), value=1, step=1, format=None,key=6)					
 					with col44:
-						dl  = st.form_submit_button('👉*_Clear ALL_*'type=secondary)
+						dl  = st.form_submit_button('👉*_Clear ALL_*',type=secondary)
 					with placeholder101.container():
 						st.write(f'<h1 style="color:#33ff33;font-size:40px;">{f"Complete Trade"}</h1>', unsafe_allow_html=True)
 						B = df5.style.format(subset=["ENTRY","QTY","STOPLOSS","TARGET","LTP","P_L" ], formatter="{:.2f}").applymap(col)					
