5841ae76289ff46ee9613d64a0f9f7eb5587d0a8
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-05 14:49:56 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -155,15 +155,16 @@ with st.form("opt_form"):
 			try:
 				for i in df['STOCK']:
 					m = alice.get_scrip_info(alice.get_instrument_by_symbol('NFO',i))				
-					lt = m['LTP']								
-					em.append(lt)
+					lt = float(m['LTP'])
+					df.loc[3, ['Price']] = ['lt'] 
+					#em.append(lt)
 			except Exception as e:
 				st.write(f"Er.",{e})					
 			#df100 = pd.DataFrame()
-			df1 = pd.Series(em,name='LTP').astype('float')
+			#df1 = pd.Series(em,name='LTP').astype('float')
 			st.write(df1.dtypes)
-			df = pd.concat([df,df1],axis=1)
-			st.write(df.dtypes)
+			#df = pd.concat([df,df1],axis=1)
+			#st.write(df.dtypes)
 			df['P_L']  = ((df['LTP'] - df['ENTRY']) * df['QTY'])
 			M = df['ENTRY'] * df['QTY']				
 			with placeholder12.container():					
