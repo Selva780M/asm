f10e357c472f5f9e2460d8d9be30300de9cf5ae3
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-05 17:20:44 +0530

    Update uc.py

--- uc.py
+++ uc.py
@@ -178,35 +178,35 @@ with st.form("opt_form"):
 					with col7:
 						st.metric("%",f"{round(((im()/30000)*100),1)}%" , f"{round(((PL/30000)*100),1)}%")
 					#st.download_button(label='📥 Download File', data=df5.to_csv(), file_name="PaperTrade.csv", mime='csv',key=7)
-						for i in range(0,len(df100.index)):					
-							if(df100.iloc[i,7]) > (df100.iloc[i,6]) and (df100.iloc[i,0] not in df5['DATE'].tolist()):
-								df2 = {"DATE" : df100.iloc[i]['DATE'] ,"NAME": df100.iloc[i]['NAME'], "STOCK" : df100.iloc[i]['STOCK'],  "ENTRY" : df100.iloc[i]['ENTRY'], "QTY" : df100.iloc[i]['QTY'], "STOPLOSS" : df100.iloc[i]['STOPLOSS'], "TARGET" : df10.iloc[i]['TARGET'], "LTP" : df100.iloc[i]['LTP'],"P_L" :df100.iloc[i]['P_L']}						
-								df5 = df5.append(df2, ignore_index = True)
-								df5.to_csv('trade.csv',index = False)
-								st.balloons()
-								df.drop([i], inplace = True)
-							if(df100.iloc[i,7]) < (df100.iloc[i,5]) and (df100.iloc[i,0] not in df5['DATE'].tolist()):											
-								df3 = {"DATE" : df100.iloc[i]['DATE'] ,"NAME": df100.iloc[i]['NAME'], "STOCK" : df100.iloc[i]['STOCK'],  "ENTRY" : df100.iloc[i]['ENTRY'], "QTY" : df100.iloc[i]['QTY'], "STOPLOSS" : df100.iloc[i]['STOPLOSS'], "TARGET" : df100.iloc[i]['TARGET'], "LTP" : df100.iloc[i]['LTP'],"P_L" :df100.iloc[i]['P_L']}
-								df5 = df5.append(df3, ignore_index = True)
-								df5.to_csv('trade.csv',index = False)
-								st.balloons()
-								df.drop([i], inplace = True)
-				with col33:
-					with placeholder01.container():
-						n1 = alice.get_scrip_info(alice.get_instrument_by_symbol("INDICES","NIFTY 50"))
-						b1= alice.get_scrip_info(alice.get_instrument_by_symbol("INDICES","NIFTY BANK"))
-						n5 = n1['LTP']
-						b5 = b1['LTP']
-						st.subheader(f'*_Nifty- 50 Spot Price :green[{n5}]_* ⏰')
-						st.subheader(f'*_BankNifty Spot Price :green[{b5}]_* ⏰')		
-					with placeholder100.container():
-						st.write(f'<h1 style="color:#33ff33;font-size:40px;">{f"Position"}</h1>', unsafe_allow_html=True)					
-						A = df100.style.format(subset=["ENTRY","QTY","STOPLOSS","TARGET","LTP","P_L" ], formatter="{:.2f}").applymap(col)
-						st.table(A)
-					with placeholder101.container():
-						st.write(f'<h1 style="color:#33ff33;font-size:40px;">{f"Complete Trade"}</h1>', unsafe_allow_html=True)
-						B = df5.style.format(subset=["ENTRY","QTY","STOPLOSS","TARGET","LTP","P_L" ], formatter="{:.2f}").applymap(col)					
-						st.table(B)					
+				for i in range(0,len(df100.index)):					
+					if(df100.iloc[i,7]) > (df100.iloc[i,6]) and (df100.iloc[i,0] not in df5['DATE'].tolist()):
+						df2 = {"DATE" : df100.iloc[i]['DATE'] ,"NAME": df100.iloc[i]['NAME'], "STOCK" : df100.iloc[i]['STOCK'],  "ENTRY" : df100.iloc[i]['ENTRY'], "QTY" : df100.iloc[i]['QTY'], "STOPLOSS" : df100.iloc[i]['STOPLOSS'], "TARGET" : df10.iloc[i]['TARGET'], "LTP" : df100.iloc[i]['LTP'],"P_L" :df100.iloc[i]['P_L']}						
+						df5 = df5.append(df2, ignore_index = True)
+						df5.to_csv('trade.csv',index = False)
+						st.balloons()
+						df.drop([i], inplace = True)
+					if(df100.iloc[i,7]) < (df100.iloc[i,5]) and (df100.iloc[i,0] not in df5['DATE'].tolist()):											
+						df3 = {"DATE" : df100.iloc[i]['DATE'] ,"NAME": df100.iloc[i]['NAME'], "STOCK" : df100.iloc[i]['STOCK'],  "ENTRY" : df100.iloc[i]['ENTRY'], "QTY" : df100.iloc[i]['QTY'], "STOPLOSS" : df100.iloc[i]['STOPLOSS'], "TARGET" : df100.iloc[i]['TARGET'], "LTP" : df100.iloc[i]['LTP'],"P_L" :df100.iloc[i]['P_L']}
+						df5 = df5.append(df3, ignore_index = True)
+						df5.to_csv('trade.csv',index = False)
+						st.balloons()
+						df.drop([i], inplace = True)
+	with col33:
+		with placeholder01.container():
+			n1 = alice.get_scrip_info(alice.get_instrument_by_symbol("INDICES","NIFTY 50"))
+			b1= alice.get_scrip_info(alice.get_instrument_by_symbol("INDICES","NIFTY BANK"))
+			n5 = n1['LTP']
+			b5 = b1['LTP']
+			st.subheader(f'*_Nifty- 50 Spot Price :green[{n5}]_* ⏰')
+			st.subheader(f'*_BankNifty Spot Price :green[{b5}]_* ⏰')		
+				with placeholder100.container():
+					st.write(f'<h1 style="color:#33ff33;font-size:40px;">{f"Position"}</h1>', unsafe_allow_html=True)					
+					A = df100.style.format(subset=["ENTRY","QTY","STOPLOSS","TARGET","LTP","P_L" ], formatter="{:.2f}").applymap(col)
+					st.table(A)
+				with placeholder101.container():
+					st.write(f'<h1 style="color:#33ff33;font-size:40px;">{f"Complete Trade"}</h1>', unsafe_allow_html=True)
+					B = df5.style.format(subset=["ENTRY","QTY","STOPLOSS","TARGET","LTP","P_L" ], formatter="{:.2f}").applymap(col)					
+					st.table(B)					
 				df.to_csv('token.csv',index = False)
 				time.sleep(1)
 
