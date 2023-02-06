be756794a840d485eccdf3290959942118fe29a5
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-06 10:39:06 +0530

    Update papertrade.py

--- papertrade.py
+++ papertrade.py
@@ -213,8 +213,11 @@ if x =="Report":
 						st.subheader(f'*_BankNifty Spot Price :green[{b5}]_* ⏰')		
 				with placeholder100.container():
 					st.write(f'<h1 style="color:#33ff33;font-size:40px;">{f"Position"}</h1>', unsafe_allow_html=True)					
-					A = df100.style.format(subset=["ENTRY","QTY","STOPLOSS","TARGET","LTP","P_L" ], formatter="{:.2f}").applymap(col)
-					st.table(A)
+					if len(df['STOCK']) < 0:
+						st.title("No Position Order")					
+					else:
+						A = df100.style.format(subset=["ENTRY","QTY","STOPLOSS","TARGET","LTP","P_L" ], formatter="{:.2f}").applymap(col)
+						st.table(A)
 				with placeholder101.container():
 					st.write(f'<h1 style="color:#33ff33;font-size:40px;">{f"Complete Trade"}</h1>', unsafe_allow_html=True)
 					B = df5.style.format(subset=["ENTRY","QTY","STOPLOSS","TARGET","LTP","P_L" ], formatter="{:.2f}").applymap(col)					
