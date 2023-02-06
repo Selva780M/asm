86d93b9f23af355f55f995a0ed27bc98dc76cb47
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-06 09:49:23 +0530

    Update papertrade.py

--- papertrade.py
+++ papertrade.py
@@ -214,7 +214,7 @@ if x =="Report":
 						st.write(f'<h1 style="color:#33ff33;font-size:40px;">{f"Complete Trade"}</h1>', unsafe_allow_html=True)
 						B = df5.style.format(subset=["ENTRY","QTY","STOPLOSS","TARGET","LTP","P_L" ], formatter="{:.2f}").applymap(col)					
 						st.table(B)					
-					df.to_csv('token.csv',index = False)
+					#df.to_csv('token.csv',index = False)
 				time.sleep(1)
 
 
