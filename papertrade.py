2dcc5e77195b5edf2f1c61e27905d31a6244e44d
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-06 10:19:20 +0530

    Update papertrade.py

--- papertrade.py
+++ papertrade.py
@@ -82,7 +82,7 @@ x = st.sidebar.radio('*_Main Page_*',("Order Placed","Report"))
 if x =="Order Placed" :
 	with st.form("opt_form"):	
 		col11, col22, col33 = st.columns(3)	
-		user_USER = st.sidebar.radio('*_Strategy_*',("Price action","ORB Day","ORB 930","BTST","STBT","test"),key=1)
+		user_USER = st.radio('*_Strategy_*',("Price action","ORB Day","ORB 930","BTST","STBT","test"),horizontal=True,key=1)
 		st.sidebar.write(f'<h1 style="color:#33ff33;font-size:30px;">{f" {user_USER} 👋"}</h1>', unsafe_allow_html=True)	
 		with col11:		
 			user_STOCK = st.radio("*_Stock (Current strike)_*",("NIFTY","BANKNIFTY"), horizontal=True,key=2)
@@ -149,7 +149,7 @@ if x =="Report":
 	placeholder101 = st.empty()	
 	if len(df['STOCK']) > 0:
 		while True:
-			#df.to_csv('token.csv',index = False)
+			df.to_csv('token.csv',index = False)
 			em = []
 			try:
 				for i in df['STOCK']:
@@ -191,16 +191,16 @@ if x =="Report":
 					df5.to_csv('trade.csv',index = False)					
 					df.drop([i], inplace = True)
 					st.balloons()
-					time.sleep(1)
-					df.to_csv('token.csv',index = False)
+					#time.sleep(1)
+					#df.to_csv('token.csv',index = False)
 				if(df100.iloc[i,7]) < (df100.iloc[i,5]) and (df100.iloc[i,0] not in df5['DATE'].tolist()):											
 					df3 = {"DATE" : df100.iloc[i]['DATE'] ,"NAME": df100.iloc[i]['NAME'], "STOCK" : df100.iloc[i]['STOCK'],  "ENTRY" : df100.iloc[i]['ENTRY'], "QTY" : df100.iloc[i]['QTY'], "STOPLOSS" : df100.iloc[i]['STOPLOSS'], "TARGET" : df100.iloc[i]['TARGET'], "LTP" : df100.iloc[i]['LTP'],"P_L" :df100.iloc[i]['P_L']}
 					df5 = df5.append(df3, ignore_index = True)
 					df5.to_csv('trade.csv',index = False)
 					df.drop([i], inplace = True)
 					st.balloons()
-					time.sleep(1)
-					df.to_csv('token.csv',index = False)
+					#time.sleep(1)
+					#df.to_csv('token.csv',index = False)
 				with placeholder01.container():
 					col1, col2 = st.columns(2)
 					with col1:
