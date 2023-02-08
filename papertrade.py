d7d9ab83eede61f235e46ee8fea279554788a173
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-08 18:21:10 +0530

    Update papertrade.py

--- papertrade.py
+++ papertrade.py
@@ -277,7 +277,7 @@ if x =="Report":
 				with col7:
 					st.metric("%",f"{round(((im()/Investment)*100),1)}%" , f"{round(((PL/Investment)*100),1)}%")				
 			for i in range(0,len(df100.index)):
-				if (df100.iloc[i,'TRADE']) == "B":
+				if (df100.iloc[i,4]) == "B":
 					if(df100.iloc[i,8]) > (df100.iloc[i,7]) and (df100.iloc[i,0] not in df5['DATE'].tolist()):
 						df2 = {"DATE" : df100.iloc[i]['DATE'] ,"NAME": df100.iloc[i]['NAME'], "STOCK" : df100.iloc[i]['STOCK'], "EXCH":df100.iloc[i]['EXCH'], "ENTRY" : df100.iloc[i]['ENTRY'], "QTY" : df100.iloc[i]['QTY'], "STOPLOSS" : df100.iloc[i]['STOPLOSS'], "TARGET" : df100.iloc[i]['TARGET'], "LTP" : df100.iloc[i]['LTP'],"P_L" :df100.iloc[i]['P_L']}						
 						df5 = df5.append(df2, ignore_index = True)
@@ -294,7 +294,7 @@ if x =="Report":
 						st.balloons()
 						send_sticker_on_telegram(sad)
 						send_msg_on_telegram(f"Sorry {Na}, Your Trade  {df100.iloc[i]['NAME']}  Completed Lose in Rs.{round(df100.iloc[i]['P_L'],1)}")						
-				if (df100.iloc[i,'TRADE']) == "S":
+				if (df100.iloc[i,4]) == "S":
 					if(df100.iloc[i,8]) < (df100.iloc[i,7]) and (df100.iloc[i,0] not in df5['DATE'].tolist()):
 						df2 = {"DATE" : df100.iloc[i]['DATE'] ,"NAME": df100.iloc[i]['NAME'], "STOCK" : df100.iloc[i]['STOCK'], "EXCH":df100.iloc[i]['EXCH'], "ENTRY" : df100.iloc[i]['ENTRY'], "QTY" : df100.iloc[i]['QTY'], "STOPLOSS" : df100.iloc[i]['STOPLOSS'], "TARGET" : df100.iloc[i]['TARGET'], "LTP" : df100.iloc[i]['LTP'],"P_L" :df100.iloc[i]['P_L']}						
 						df5 = df5.append(df2, ignore_index = True)
