8d1a0c61423e45e9697d4d09350dbc5f35fbcf75
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-02-09 08:09:22 +0530

    Update papertrade.py

--- papertrade.py
+++ papertrade.py
@@ -127,7 +127,7 @@ if x =="Order Placed" :
 	user = st.radio('*_Choose the Stock_*',("Auto","Manual"),horizontal=True,key=1)	
 	if user == "Manual":
 		XX = st.radio("*_Select Exchange_*",("NFO","NSE","CDS","MCX"),horizontal=True,key=3)
-		dis = st.radio('*_Option Chain_*',("Yes","No"),horizontal=True,key=2)
+		dis = st.radio('*_Option Chain_*',("Yes","No"),horizontal=True,key=12)
 	with st.form("opt_form"):				
 		user_USER = st.radio('*_Strategy_*',("Price action","ORB Day","ORB 930","BTST","STBT","Hedging","test"),horizontal=True,key=2)
 		st.sidebar.write(f'<h1 style="color:#33ff33;font-size:30px;">{f" {user_USER} 👋"}</h1>', unsafe_allow_html=True)
