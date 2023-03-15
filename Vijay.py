f322e86ca03145effec4b1885f51d8ca85d64317
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-03-15 12:42:36 +0530

    Update Vijay.py

--- Vijay.py
+++ Vijay.py
@@ -304,7 +304,7 @@ if x =="Report":
 						df.drop([i], inplace = True)
 						st.balloons()						
 						send_sticker_on_telegram(happy)
-						send_msg_on_telegram(f"Hi {Na}, Your Trade {df100.iloc[i]['NAME']} Completed, Profit in Rs.{round(df100.iloc[i]['P_L'],1)}")						
+						send_msg_on_telegram(f"Hi {Na}, \nYour Trade  {df100.iloc[i]['NAME']} Completed, Profit in Rs.{round(df100.iloc[i]['P_L'],1)}\nDATE : {df100.iloc[i]['DATE']} \nSTOCK : {df100.iloc[i]['STOCK']} \nTRADE: {df100.iloc[i]['TRADE']} \nEntry Price: {df100.iloc[i]['ENTRY']}\nQTY: {df100.iloc[i]['QTY']}\nSTOPLOSS : {df100.iloc[i]['STOPLOSS']}\nTARGET : {df100.iloc[i]['TARGET']}\nProfit_Loss : {round(df100.iloc[i]['P_L'],1)}")
 				if (df100.iloc[i,9]) > (df100.iloc[i,7]) and (df100.iloc[i,0] not in df5['DATE'].tolist()):
 					if (df100.iloc[i,4]) == "S":
 						df3 = {"DATE" : df100.iloc[i]['DATE'] ,"NAME": df100.iloc[i]['NAME'], "STOCK" : df100.iloc[i]['STOCK'],"EXCH":df100.iloc[i]['EXCH'], "TRADE": df100.iloc[i]['TRADE'],"ENTRY" : df100.iloc[i]['ENTRY'], "QTY" : df100.iloc[i]['QTY'], "STOPLOSS" : df100.iloc[i]['STOPLOSS'], "TARGET" : df100.iloc[i]['TARGET'], "LTP" : df100.iloc[i]['LTP'],"P_L" :df100.iloc[i]['P_L']}
@@ -313,7 +313,7 @@ if x =="Report":
 						df.drop([i], inplace = True)
 						st.balloons()						
 						send_sticker_on_telegram(sad)
-						send_msg_on_telegram(f"Sorry {Na}, Your Trade  {df100.iloc[i]['NAME']}  Completed Lose in Rs.{round(df100.iloc[i]['P_L'],1)}")			
+						send_msg_on_telegram(f"Hi {Na}, \nYour Trade {df100.iloc[i]['NAME']} Completed, Lose in Rs.{round(df100.iloc[i]['P_L'],1)}\nDATE : {df100.iloc[i]['DATE']} \nSTOCK : {df100.iloc[i]['STOCK']} \nTRADE: {df100.iloc[i]['TRADE']} \nEntry Price: {df100.iloc[i]['ENTRY']}\nQTY: {df100.iloc[i]['QTY']}\nSTOPLOSS : {df100.iloc[i]['STOPLOSS']}\nTARGET : {df100.iloc[i]['TARGET']}\nProfit_Loss : {round(df100.iloc[i]['P_L'],1)}")			
 			with placeholder01.container():
 				col1, col2 = st.columns(2)
 				with col1:
