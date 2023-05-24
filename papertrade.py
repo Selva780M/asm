1ee9c9e848f852b7777e8e5f0fcc43b960041242
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-05-24 12:07:36 +0530

    Update papertrade.py

--- papertrade.py
+++ papertrade.py
@@ -394,7 +394,7 @@ if x =="Report":
 				st.table(B)
 				st.warning('*_Paper Trade Payoff Chart_*')
 				st.write(f'<iframe src="https://nifty50signal.streamlit.app/" frameborder="0" scrolling="no" webkitAllowFullScreen="true" mozallowfullscreen="true" allowFullScreen="true" height="400" width="100%"></iframe>',unsafe_allow_html=True)
-				if user_STOCK == "BANKNIFTY":
+				if df100.iloc[-1]['NAME'] == "Hedging":
 					op1={'op_type': 'c', 'strike': spot_prc, 'tr_type': 's', 'op_pr': 7.63}
 					op2={'op_type': 'p', 'strike': spot_prc, 'tr_type': 's', 'op_pr': 5.35}
 					op3={'op_type': 'c', 'strike': (spot_prc+50), 'tr_type': 'b', 'op_pr': 7.20}
