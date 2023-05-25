4f7d231787e35110c7d0f5ddd5b503d947558f32
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-05-25 10:53:51 +0530

    Update papertrade.py

--- papertrade.py
+++ papertrade.py
@@ -393,8 +393,7 @@ if x =="Report":
 				B = df5.style.format(subset=["ENTRY","QTY","STOPLOSS","TARGET","LTP","P_L" ], formatter="{:.2f}").applymap(col)					
 				st.table(B)
 				st.warning('*_Paper Trade Payoff Chart_*')				
-				with st.form("opt_form"):
-					user_STOCK = st.radio("*_Stock (Current strike)_*",("FINNIFTY","BANKNIFTY","NIFTY"), horizontal=True,key=5)				
+				user_STOCK = st.radio("*_Stock (Current strike)_*",("FINNIFTY","BANKNIFTY","NIFTY"), horizontal=True,key=5)				
 				if (df100.iloc[0,1]) > 0 :
 					if user_STOCK == "FINNIFTY":
 						b1= alice.get_scrip_info(alice.get_instrument_by_symbol("INDICES","NIFTY FIN SERVICE"))						
