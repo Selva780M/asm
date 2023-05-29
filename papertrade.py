4fb70ce9b4aea1ae6978d2178a8b34f7e3b12bc6
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-05-29 15:06:49 +0530

    Update papertrade.py

--- papertrade.py
+++ papertrade.py
@@ -137,7 +137,7 @@ def algo(stok,spot,qt,OP,expiry_date,T):
 	df = pd.concat([df, pd.DataFrame([new_data])], ignore_index=True)
 	stok = ""
 	temp()
-x = st.sidebar.radio('*_Main Page_*',("Order Placed","Report","Access File"),key=1)
+x = st.sidebar.radio('*_Main Page_*',("Order Placed","Report","Access File","payoff-chart"),key=1)
 if x =="Order Placed" :
 	user = st.radio('*_Choose the Stock_*',("Auto","Manual","Rjalgo"),horizontal=True,key=2)	
 	if user == "Manual":
@@ -298,7 +298,7 @@ if x =="Order Placed" :
 if x =="Report":
 	placeholder01 = st.empty()
 	placeholder12 = st.sidebar.empty()
-	placeholder13 = st.sidebar.empty()	
+	placeholder13 = st.sidebar.empty()
 	placeholder100 = st.empty()
 	placeholder101 = st.empty()	
 	if (len(df5['STOCK']) > -1) | (len(df['STOCK']) > -1):
@@ -407,55 +407,6 @@ if x =="Report":
 					else:
 						A = df100.style.format(subset=["ENTRY","QTY","STOPLOSS","TARGET","LTP","P_L" ], formatter="{:.2f}").applymap(col)
 						st.table(A)
-						st.success('*_pay-off Chart_*')
-						col01, col02 = st.columns(2)
-						with col01:
-							user_STOCK = st.radio("*_Stock (Current strike)_*",("FINNIFTY","BANKNIFTY","NIFTY"), horizontal=True,key=11)
-						with col02:
-							sprange = st.number_input('*_Spot Range_*', min_value=0.1, max_value=100.0, value=0.5, step=0.1, format=None,key=17)
-						gb = GridOptionsBuilder.from_dataframe(df100)
-						gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
-						gb.configure_side_bar() #Add a sidebar
-						gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
-						gridOptions = gb.build()
-						grid_response = AgGrid(df100,gridOptions=gridOptions,data_return_mode='AS_INPUT',update_mode='MODEL_CHANGED',fit_columns_on_grid_load=False,theme='alpine',enable_enterprise_modules=True,height=350,width='100%',reload_data=True)
-						data = grid_response['data']
-						selected = grid_response['selected_rows'] 
-						df10 = pd.DataFrame(selected) #Pass the selected rows to a new dataframe df
-						st.table(df10)
-						try:
-							if user_STOCK == "FINNIFTY":
-								b1= alice.get_scrip_info(alice.get_instrument_by_symbol("INDICES","NIFTY FIN SERVICE"))						
-								b5 = b1['LTP']
-								op1={'op_type': 'c', 'strike': df10.iloc[0,1], 'tr_type': 's', 'op_pr': int(df10.iloc[0,10])}
-								op2={'op_type': 'p', 'strike': df10.iloc[1,1], 'tr_type': 's', 'op_pr': int(df10.iloc[1,10])}
-								op3={'op_type': 'c', 'strike': df10.iloc[2,1], 'tr_type': 'b', 'op_pr': int(df10.iloc[2,10])}
-								op4={'op_type': 'p', 'strike': df10.iloc[3,1], 'tr_type': 'b', 'op_pr': int(df10.iloc[3,10])}
-								op_list = [op1, op2, op3, op4]
-								fig = op.multi_plotter(spot=float(b5),spot_range=float(sprange), op_list=op_list)
-								st.pyplot(fig,use_container_width=True)
-							if user_STOCK == "BANKNIFTY":
-								b1= alice.get_scrip_info(alice.get_instrument_by_symbol("INDICES","NIFTY BANK"))						
-								b5 = b1['LTP']
-								op1={'op_type': 'c', 'strike': df10.iloc[0,1], 'tr_type': 's', 'op_pr': df10.iloc[0,9]}
-								op2={'op_type': 'p', 'strike': df10.iloc[1,1], 'tr_type': 's', 'op_pr': df10.iloc[1,9]}
-								op3={'op_type': 'c', 'strike': df10.iloc[2,1], 'tr_type': 'b', 'op_pr': df10.iloc[2,9]}
-								op4={'op_type': 'p', 'strike': df10.iloc[3,1], 'tr_type': 'b', 'op_pr': df10.iloc[3,9]}
-								op_list = [op1, op2, op3, op4]
-								fig = op.multi_plotter(spot=float(b5), spot_range=float(sprange),op_list=op_list)
-								st.pyplot(fig,use_container_width=True)
-							if user_STOCK == "NIFTY":
-								b1= alice.get_scrip_info(alice.get_instrument_by_symbol("INDICES","NIFTY 50"))						
-								b5 = b1['LTP']
-								op1={'op_type': 'c', 'strike': df10.iloc[0,1], 'tr_type': 's', 'op_pr':df10.iloc[0,9]}
-								op2={'op_type': 'p', 'strike': df10.iloc[1,1], 'tr_type': 's', 'op_pr': df10.iloc[1,9]}
-								op3={'op_type': 'c', 'strike': df10.iloc[2,1], 'tr_type': 'b', 'op_pr': df10.iloc[2,9]}
-								op4={'op_type': 'p', 'strike': df10.iloc[3,1], 'tr_type': 'b', 'op_pr': df10.iloc[3,9]}
-								op_list = [op1, op2, op3, op4]
-								fig = op.multi_plotter(spot=float(b5),spot_range=float(sprange),op_list=op_list)
-								st.pyplot(fig,use_container_width=True)
-						except Exception as e:
-							st.title(f'Not Pay-off Chart and Error on {e}')
 				with placeholder101.container():
 					st.info('*_Paper Trade Result_*')				
 					B = df5.style.format(subset=["ENTRY","QTY","STOPLOSS","TARGET","LTP","P_L" ], formatter="{:.2f}").applymap(col)					
@@ -480,7 +431,64 @@ if x == "Access File":
 			temp()		
 		A = df.style.format(subset=["ENTRY","QTY","STOPLOSS","TARGET"], formatter="{:.2f}").applymap(col)
 		st.table(A)
-
+if x == "payoff-chart":	
+	placeholder100 = st.empty()
+	with placeholder100.container():
+		st.success('*_payoff-chart_*')									
+		if len(df100['STOCK']) < 0:
+			st.title("No Position Order")					
+		else:
+			col01, col02 = st.columns(2)
+			with col01:
+				user_STOCK = st.radio("*_Stock (Current strike)_*",("FINNIFTY","BANKNIFTY","NIFTY"), horizontal=True,key=11)
+			with col02:
+				sprange = st.number_input('*_Spot Range_*', min_value=0.1, max_value=100.0, value=0.5, step=0.1, format=None,key=17)
+				gb = GridOptionsBuilder.from_dataframe(df100)
+				gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
+				gb.configure_side_bar() #Add a sidebar
+				gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
+				gridOptions = gb.build()
+				grid_response = AgGrid(df100,gridOptions=gridOptions,data_return_mode='AS_INPUT',update_mode='MODEL_CHANGED',fit_columns_on_grid_load=False,theme='alpine',enable_enterprise_modules=True,height=350,width='100%',reload_data=True)
+				data = grid_response['data']
+				selected = grid_response['selected_rows'] 
+				df10 = pd.DataFrame(selected) #Pass the selected rows to a new dataframe df
+				st.table(df10)
+				try:
+					if user_STOCK == "FINNIFTY":
+						b1= alice.get_scrip_info(alice.get_instrument_by_symbol("INDICES","NIFTY FIN SERVICE"))						
+						b5 = b1['LTP']
+						op1={'op_type': 'c', 'strike': df10.iloc[0,1], 'tr_type': 's', 'op_pr': int(df10.iloc[0,10])}
+						op2={'op_type': 'p', 'strike': df10.iloc[1,1], 'tr_type': 's', 'op_pr': int(df10.iloc[1,10])}
+						op3={'op_type': 'c', 'strike': df10.iloc[2,1], 'tr_type': 'b', 'op_pr': int(df10.iloc[2,10])}
+						op4={'op_type': 'p', 'strike': df10.iloc[3,1], 'tr_type': 'b', 'op_pr': int(df10.iloc[3,10])}
+						op_list = [op1, op2, op3, op4]
+						fig = op.multi_plotter(spot=float(b5),spot_range=float(sprange), op_list=op_list)
+						st.pyplot(fig,use_container_width=True)
+					if user_STOCK == "BANKNIFTY":
+						b1= alice.get_scrip_info(alice.get_instrument_by_symbol("INDICES","NIFTY BANK"))						
+						b5 = b1['LTP']
+						op1={'op_type': 'c', 'strike': df10.iloc[0,1], 'tr_type': 's', 'op_pr': df10.iloc[0,9]}
+						op2={'op_type': 'p', 'strike': df10.iloc[1,1], 'tr_type': 's', 'op_pr': df10.iloc[1,9]}
+						op3={'op_type': 'c', 'strike': df10.iloc[2,1], 'tr_type': 'b', 'op_pr': df10.iloc[2,9]}
+						op4={'op_type': 'p', 'strike': df10.iloc[3,1], 'tr_type': 'b', 'op_pr': df10.iloc[3,9]}
+						op_list = [op1, op2, op3, op4]
+						fig = op.multi_plotter(spot=float(b5), spot_range=float(sprange),op_list=op_list)
+						st.pyplot(fig,use_container_width=True)
+					if user_STOCK == "NIFTY":
+						b1= alice.get_scrip_info(alice.get_instrument_by_symbol("INDICES","NIFTY 50"))						
+						b5 = b1['LTP']
+						op1={'op_type': 'c', 'strike': df10.iloc[0,1], 'tr_type': 's', 'op_pr':df10.iloc[0,9]}
+						op2={'op_type': 'p', 'strike': df10.iloc[1,1], 'tr_type': 's', 'op_pr': df10.iloc[1,9]}
+						op3={'op_type': 'c', 'strike': df10.iloc[2,1], 'tr_type': 'b', 'op_pr': df10.iloc[2,9]}
+						op4={'op_type': 'p', 'strike': df10.iloc[3,1], 'tr_type': 'b', 'op_pr': df10.iloc[3,9]}
+						op_list = [op1, op2, op3, op4]
+						fig = op.multi_plotter(spot=float(b5),spot_range=float(sprange),op_list=op_list)
+						st.pyplot(fig,use_container_width=True)
+				except Exception as e:
+					st.title(f'Not Pay-off Chart and Error on {e}')
+	
+	
+	
 	
 	
 	
