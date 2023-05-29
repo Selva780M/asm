4fb2e42024f060ba2e60440e16686dd07ba357f6
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-05-29 15:27:25 +0530

    Update papertrade.py

--- papertrade.py
+++ papertrade.py
@@ -456,38 +456,38 @@ if x == "payoff-chart":
 				data = grid_response['data']
 				selected = grid_response['selected_rows'] 
 				df10 = pd.DataFrame(selected) #Pass the selected rows to a new dataframe df
-				st.table(df10)
-				try:
-					if user_STOCK == "FINNIFTY":
-						b1= alice.get_scrip_info(alice.get_instrument_by_symbol("INDICES","NIFTY FIN SERVICE"))						
-						b5 = b1['LTP']
-						op1={'op_type': 'c', 'strike': df10.iloc[0,2], 'tr_type': 's', 'op_pr': int(df10.iloc[0,10])}
-						op2={'op_type': 'p', 'strike': df10.iloc[1,2], 'tr_type': 's', 'op_pr': int(df10.iloc[1,10])}
-						op3={'op_type': 'c', 'strike': df10.iloc[2,2], 'tr_type': 'b', 'op_pr': int(df10.iloc[2,10])}
-						op4={'op_type': 'p', 'strike': df10.iloc[3,2], 'tr_type': 'b', 'op_pr': int(df10.iloc[3,10])}
-						op_list = [op1, op2, op3, op4]
-						fig = op.multi_plotter(spot=float(b5),spot_range=float(sprange), op_list=op_list)
-						st.pyplot(fig,use_container_width=True)
-					if user_STOCK == "BANKNIFTY":
-						b1= alice.get_scrip_info(alice.get_instrument_by_symbol("INDICES","NIFTY BANK"))						
-						b5 = b1['LTP']
-						op1={'op_type': 'c', 'strike': df10.iloc[0,1], 'tr_type': 's', 'op_pr': df10.iloc[0,9]}
-						op2={'op_type': 'p', 'strike': df10.iloc[1,1], 'tr_type': 's', 'op_pr': df10.iloc[1,9]}
-						op3={'op_type': 'c', 'strike': df10.iloc[2,1], 'tr_type': 'b', 'op_pr': df10.iloc[2,9]}
-						op4={'op_type': 'p', 'strike': df10.iloc[3,1], 'tr_type': 'b', 'op_pr': df10.iloc[3,9]}
-						op_list = [op1, op2, op3, op4]
-						fig = op.multi_plotter(spot=float(b5), spot_range=float(sprange),op_list=op_list)
-						st.pyplot(fig,use_container_width=True)
-					if user_STOCK == "NIFTY":
-						b1= alice.get_scrip_info(alice.get_instrument_by_symbol("INDICES","NIFTY 50"))						
-						b5 = b1['LTP']
-						op1={'op_type': 'c', 'strike': df10.iloc[0,1], 'tr_type': 's', 'op_pr':df10.iloc[0,9]}
-						op2={'op_type': 'p', 'strike': df10.iloc[1,1], 'tr_type': 's', 'op_pr': df10.iloc[1,9]}
-						op3={'op_type': 'c', 'strike': df10.iloc[2,1], 'tr_type': 'b', 'op_pr': df10.iloc[2,9]}
-						op4={'op_type': 'p', 'strike': df10.iloc[3,1], 'tr_type': 'b', 'op_pr': df10.iloc[3,9]}
-						op_list = [op1, op2, op3, op4]
-						fig = op.multi_plotter(spot=float(b5),spot_range=float(sprange),op_list=op_list)
-						st.pyplot(fig,use_container_width=True)
+			st.table(df10)
+			try:
+				if user_STOCK == "FINNIFTY":
+					b1= alice.get_scrip_info(alice.get_instrument_by_symbol("INDICES","NIFTY FIN SERVICE"))						
+					b5 = b1['LTP']
+					op1={'op_type': 'c', 'strike': df10.iloc[0,2], 'tr_type': 's', 'op_pr': df10.iloc[0,10]}
+					op2={'op_type': 'p', 'strike': df10.iloc[1,2], 'tr_type': 's', 'op_pr': df10.iloc[1,10]}
+					op3={'op_type': 'c', 'strike': df10.iloc[2,2], 'tr_type': 'b', 'op_pr': df10.iloc[2,10]}
+					op4={'op_type': 'p', 'strike': df10.iloc[3,2], 'tr_type': 'b', 'op_pr': df10.iloc[3,10]}
+					op_list = [op1, op2, op3, op4]
+					fig = op.multi_plotter(spot=float(b5),spot_range=float(sprange), op_list=op_list)
+					st.pyplot(fig,use_container_width=True)
+				if user_STOCK == "BANKNIFTY":
+					b1= alice.get_scrip_info(alice.get_instrument_by_symbol("INDICES","NIFTY BANK"))						
+					b5 = b1['LTP']
+					op1={'op_type': 'c', 'strike': df10.iloc[0,2], 'tr_type': 's', 'op_pr': df10.iloc[0,10]}
+					op2={'op_type': 'p', 'strike': df10.iloc[1,2], 'tr_type': 's', 'op_pr': df10.iloc[1,10]}
+					op3={'op_type': 'c', 'strike': df10.iloc[2,2], 'tr_type': 'b', 'op_pr': df10.iloc[2,10]}
+					op4={'op_type': 'p', 'strike': df10.iloc[3,2], 'tr_type': 'b', 'op_pr': df10.iloc[3,10]}
+					op_list = [op1, op2, op3, op4]
+					fig = op.multi_plotter(spot=float(b5), spot_range=float(sprange),op_list=op_list)
+					st.pyplot(fig,use_container_width=True)
+				if user_STOCK == "NIFTY":
+					b1= alice.get_scrip_info(alice.get_instrument_by_symbol("INDICES","NIFTY 50"))						
+					b5 = b1['LTP']
+					op1={'op_type': 'c', 'strike': df10.iloc[0,2], 'tr_type': 's', 'op_pr': df10.iloc[0,10]}
+					op2={'op_type': 'p', 'strike': df10.iloc[1,2], 'tr_type': 's', 'op_pr': df10.iloc[1,10]}
+					op3={'op_type': 'c', 'strike': df10.iloc[2,2], 'tr_type': 'b', 'op_pr': df10.iloc[2,10]}
+					op4={'op_type': 'p', 'strike': df10.iloc[3,2], 'tr_type': 'b', 'op_pr': df10.iloc[3,10]}
+					op_list = [op1, op2, op3, op4]
+					fig = op.multi_plotter(spot=float(b5),spot_range=float(sprange),op_list=op_list)
+					st.pyplot(fig,use_container_width=True)
 				except Exception as e:
 					st.title(f'Not Pay-off Chart and Error on {e}')
 	
