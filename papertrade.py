f0a440059c3d474a97add46900c6fb2e7b9ffb29
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-06-01 08:41:59 +0530

    Update papertrade.py

--- papertrade.py
+++ papertrade.py
@@ -14,8 +14,8 @@ import opstrat as op
 import plotly.graph_objects as go
 day = datetime.now(timezone("Asia/Kolkata"))
 DATE = day.strftime('%d-%m-%Y %H:%M:%S')
-otp = pyotp.TOTP('TBXAQPTBGXWDKOBSIFIFZEEPMZKBQBGJ').now()
 alice = Aliceblue(user_id='627742',api_key='BPk1mFAXB9ByTFFQnm87HhieLFo3Fy5J3PCaae2g252DiLCNB9BK7hF0LpSg3d9fNO698r32IAsEt0lWm3hmuZMWW9tJC6r6A7xGkZWGmY1Hcdys1q9ITC1pRjYaklRQ')
+otp = pyotp.TOTP('TBXAQPTBGXWDKOBSIFIFZEEPMZKBQBGJ').now()
 #------------------------------------------------------
 try:
 	alice.get_session_id()
